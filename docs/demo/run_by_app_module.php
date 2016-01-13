46f9e50144fbb8a21e7be6202a842d1424de9977
kawanamiyuu <i.am.really.you@gmail.com>
2016-01-14 00:13:21 +0900

    add run_by_app_module demo

---
+++ docs/demo/run_by_app_module.php
@@ -0,0 +1,36 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Ray\Di\AbstractModule;
+use Ray\Di\Injector;
+use Ray\DoctrineOrmModule\DoctrineOrmModule;
+use Ray\DoctrineOrmModule\EntityManagerInject;
+
+$loader = require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+/* @var $loader \Composer\Autoload\ClassLoader */
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+class Fake
+{
+    use EntityManagerInject;
+
+    public function foo()
+    {
+        return $this->entityManager;
+    }
+}
+
+class AppModule extends AbstractModule
+{
+    protected function configure()
+    {
+        $this->install(new DoctrineOrmModule(['driver' => 'pdo_sqlite', 'memory' => true], []));
+    }
+}
+
+/* @var $fake Fake */
+$fake = (new Injector(new AppModule))->getInstance(Fake::class);
+
+$works = ($fake->foo() instanceof EntityManagerInterface);
+echo($works ? 'It works!' : 'It DOES NOT work!') . PHP_EOL;
