a83c68aefa4a2a4146d3ba8e50f38ea6ee643b90
kawanamiyuu <i.am.really.you@gmail.com>
2016-01-16 01:35:50 +0900

    cleanup tmp files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,13 @@ $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /* @var $loader \Composer\Autoload\ClassLoader */
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
-$_ENV['TMP_DIR'] = __DIR__.'/tmp';
+$_ENV['TMP_DIR'] = __DIR__ . '/tmp';
+
+register_shutdown_function(function () {
+    foreach (new \RecursiveDirectoryIterator($_ENV['TMP_DIR'], \FilesystemIterator::SKIP_DOTS) as $file) {
+        if ($file->getFilename() === '.placefolder') {
+            continue;
+        }
+        unlink($file);
+    }
+});
