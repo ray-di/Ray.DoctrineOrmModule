45922a379c01116e0696f7aec6e69fe38ae39ff4
kawanamiyuu <i.am.really.you@gmail.com>
2016-01-23 01:31:52 +0900

    fix failed test #4

--- tests/Logger/PsrSqlLoggerTest.php
+++ tests/Logger/PsrSqlLoggerTest.php
@@ -19,7 +19,7 @@ class PsrSqlLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(['p1', 'p2'], $logger->logs[0]['context']['params']);
         $this->assertEquals(['t1', 't2'], $logger->logs[0]['context']['types']);
 
-        $this->assertRegExp('/^query execution time: [0-9]+s$/', $logger->logs[1]['message']);
+        $this->assertRegExp('/^query execution time: ([0-9]+\.)?[0-9]+s$/', $logger->logs[1]['message']);
         $this->assertNotContains('params', $logger->logs[1]['context']);
         $this->assertNotContains('types', $logger->logs[1]['context']);
     }
